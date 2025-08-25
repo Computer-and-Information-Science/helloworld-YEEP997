cf884c94e8d0b29add5cc94d738cfb3b96bf2678
YEEP997 <deb2justin@hotmail.com>
2025-08-25 17:39:42 +0000

    created an end sequence to the code.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello \n Object \n Oriented \n Programming");
     }
 }
